63b5999d29c06222ae7e5d9052c97d980b70f21a
Kyle Desroches <kyledesroches5@gmail.com>
2025-11-20 13:32:28 -0800

    Completed home dashboard

--- frontend/expense-tracker/src/utils/helper.js
+++ frontend/expense-tracker/src/utils/helper.js
@@ -2,4 +2,41 @@ export const validateEmail = (email) => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
   };
+
+export const getInitials = (name) => {
+    if (!name) return "";
   
+    const words = name.split(" ");
+    let initials = "";
+  
+    // Take the first letter of the first two words
+    for (let i = 0; i < Math.min(2, words.length); i++) {
+      if (words[i].length > 0) {
+        initials += words[i][0].toUpperCase();
+      }
+    }
+  
+    return initials;
+  };
+
+export const addThousandsSeparator = (num) => {
+    if (num == null || isNaN(num)) return "";
+
+    // Convert number to string and split into integer and fractional parts
+    const [integerPart, fractionalPart] = num.toString().split(".");
+
+    // Add commas to the integer part using regex
+    const formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+    // Combine integer and fractional parts (if any)
+    return fractionalPart ? `${formattedInteger}.${fractionalPart}` : formattedInteger;
+  };
+
+export const prepareExpenseBarChartData = (data = []) => {
+  const chartData = data.map((item) => ({
+    category: item?.category,
+    amount: item?.amount,
+  }));
+
+  return chartData;
+};
